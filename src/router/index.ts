fad2a52eed2e537b62f9e21aae2747ab04397bd3
Ars12904578 <aac23440@gmail.com>
2026-09-13 19:12:21 +0800

    major updates

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      name: "home",
+      path: "/",
+      component: () => import("../views/HomeView.vue"),
+    },
+    {
+      name: "about",
+      path: "/about",
+      component: () => import("../views/AboutView.vue"),
+    },
+  ],
 })
 
 export default router
