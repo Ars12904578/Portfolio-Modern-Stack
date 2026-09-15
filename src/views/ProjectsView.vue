e6dfe6155ac7a3a06cb74bdabb1e9baec128a907
Ars12904578 <aac23440@gmail.com>
2026-09-15 18:29:11 +0800

    cleanup

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,7 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LiquidGlass from "@/components/LiquidGlass.vue";
+</script>
 <template>
-	<div class="flex h-full w-full items-center justify-center">
-		Under development
-	</div>
+  <div
+    class="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 h-full w-full items-center justify-center overflow-auto p-2 pb-20"
+  >
+    <div
+      class="w-full h-full min-h-60 flex items-center justify-center relative"
+      v-for="value in 1"
+      :key="value"
+    >
+      <div class="w-9/10 h-9/10 bg-white/30 rounded-4xl"></div>
+	  <LiquidGlass class="absolute bottom-1/20 inset-x-1/20 scale-100 min-h-14 p-0 rounded-4xl bg-slate-700/50 hover:scale-110 active:scale-90 cursor-pointer">UnderDevelopment</LiquidGlass>
+    </div>
+  </div>
 </template>
 <style scoped></style>
